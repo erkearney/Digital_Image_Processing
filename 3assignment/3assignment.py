9bbb5c2248bed8bb2ddf42fc5394e39f3c598e9f
eric <ericrkearney@gmail.com>
2018-10-14 19:40:31 -0600

    Serious submission worthy

--- 3assignment/3assignment.py
+++ 3assignment/3assignment.py
@@ -1,26 +1,27 @@
-"""
-Adrian Rosebrock's article "Skin Detection: A Step-by-Step Example
-using Python and OpenCV helped me immensely with this project.
+'''
+Written by Eric Kearney on 10/14/2018. This program detects skin on an input
+image and returns an image with every pixel that does not have detected skin
+set to 0. By default, This program uses the 'face_dark.bmp" image, include it
+in the same directory as this program, or supply your own image using
+3assignment,py --image <path_to_image>
 
-https://www.pyimagesearch.com/2014/08/18/skin-detection-step-step-example-using-python-opencv/
-
-This project is, in part, an implementation of the technique(s) 
-discussed in "A survey of skin-color modeling and detetction methods",
-by P. Kakumanu, S. Makrogiannis, and N. Bourbakis, published in
-"Pattern Recognition", volume #40, issue #3, pages 1106-1122 (2007).
-
-https://www.sciencedirect.com/science/article/pii/S0031320306002767
-"""
+This is my first 'serious submission', it does what it's supposed to, but. . .
+badly. I wasn't able to find a way to work with histograms directly in 
+opencv, which made the automatic thresholding VERY difficult. I truly hope
+I'll be able to take another look at this project at another time, for now,
+there is some seriously sketchy code in here, see the report for details.
+'''
 
 import cv2
 import numpy as np
 import argparse
 from pathlib import Path
 from matplotlib import pyplot as plt
+from scipy import stats
 
-default_image = "./face_dark.bmp"
+default_image = './face_dark.bmp'
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--image", help="path to the image")
+parser.add_argument('-i', '--image', help='path to the image')
 args = parser.parse_args()
 
 def validate_image():
@@ -28,56 +29,63 @@ def validate_image():
         if Path(args.image).is_file():
             img = cv2.imread(args.image)
         else:
-            print("File {} not found".format(args.image))
+            print('File {} not found'.format(args.image))
             exit()
     else:
         if Path(default_image).is_file():
             img = cv2.imread(default_image)
         else:
-            print("No image supplied, and the default image, {} " \
-                    "was not found".format(default_image))
+            print('No image supplied, and the default image, {} ' \
+                    'was not found'.format(default_image))
             exit()
     return img
 
 def create_mask(image):
-    # TODO, add histogram equalization or something
-    # For now, just using hard coded values
-    lower = np.array([0, 48, 80], dtype = "uint8")
-    upper = np.array([20, 255, 222], dtype = "uint8")
-    # Convert the image to HSV
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    # Calculuate the histogram of the luminance of this image
-    hist = cv2.calcHist([hsv], [2], None, [256], [0,256])
-    plt.plot(hist)
-    plt.show()
-    # filter out values outside of our hardcoded range
-    filtered = cv2.inRange(hsv, lower, upper)
-    # perform a gaussian blur on the mask to get rid of noise
-    gaus = cv2.GaussianBlur(filtered, (3, 3), 0)
-    return gaus
+    lower = np.array([0, 48, 80], dtype = 'uint8')
+    luminance_thresh = find_luminance_local_min(hsv)
+    upper = np.array([20, 255, luminance_thresh], dtype = 'uint8')
+    return cv2.inRange(hsv, lower, upper)
 
-def detect_skin(image):
-    img = image.copy()
-    mask = create_mask(img)
-    for r in range(0, img.shape[0]):
-        for c in range(0, img.shape[1]):
-            if mask[r][c] == 0:
-                img[r][c] = 0
+def find_luminance_local_min(hsv):
+    # Find and return the local minima between the two largest local maxima
+    # of the luminance component of the image, or at least that's what we're
+    # supposed to do here. For now, I've used the mode to find the two
+    # most frequently occuring values (aka the two highest peaks of the 
+    # histogram), and simply took the average of those two values, hoping
+    # there'd be a trough between them. To improve this, it would be necessary
+    # to make sure the two peaks aren't too close to each other. In any case,
+    # this entire approach is probably completely wrong.
+    # Get just the luminance component of the image, flatten it to a 1D 
+    # array, and sort it
+    lum = hsv[:,:,2]
+    lum = lum.ravel()
+    np.sort(lum)
+    # Find the first peak of the histogram by taking the mode of the array.
+    mode1 = stats.mode(lum)
+    # Remove all values in that array that are less than the peak, then find
+    # the next biggest peak
+    lum = lum[lum > mode1[0]]
+    mode2 = stats.mode(lum)
+    lum = lum[lum < mode2[0]]
+    return mode1[0] / 2 + mode2[0] / 2
 
-    return img
+def filter_skin(image, mask):
+    # Blur the mask to smooth the result and help get rid of the noise
+    gaus = cv2.GaussianBlur(mask, (7,7), 0)
+    return cv2.bitwise_and(image, image, mask = gaus)
 
 def main():
     img = validate_image()
+    find_luminance_local_min(img)
     mask = create_mask(img)
-    skin = detect_skin(img)
+    result = filter_skin(img, mask)
 
     cv2.imshow('original', img)
     cv2.imshow('mask', mask)
-    cv2.imshow('skin', skin)
-
-
+    cv2.imshow('result', result)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    main()
+    main()    
