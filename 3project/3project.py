a02ac92e094ef3e47b2aff36558786728188fc6e
eric <ericrkearney@gmail.com>
2018-12-09 14:16:04 -0700

    Initial setup

--- 3project/3project.py
+++ 3project/3project.py
@@ -14,13 +14,45 @@ https://metrostate-bb.blackboard.com/webapps/blackboard/content/listContent.jsp?
 As mentioned, Tovio Roberts provided tremendous help to me for this project.
 Here is his GitHub: https://github.com/clownfragment """
 
-import cv2              # For image and video analysis
-import numpy as np      # For matrix/vector manipulation
-import pandas as pd     # For reading and analyzing the GPS data
-import argparse         # For creating and handling command line arguments
+import cv2                  # For image and video analysis
+import numpy as np          # For matrix/vector manipulation
+import pandas as pd         # For reading and analyzing the GPS data
+import argparse             # For creating and handling command line arguments
+from pathlib import Path    # For reading file paths
+
+parser = argparse.ArgumentParser(
+    description="Vehicle crash and pedestrian detector built using OpenCV, numpy, and pandas"
+)
+parser.add_argument("-v", "--video", help="path to the input video")
+parser.add_argument("-d", "--data", help="path to the GPS data")
+args = parser.parse_args()
+
+def setup():
+    if args.video:
+        input_video = Path(args.video)
+        print("Using {} as the input video".format(input_video))
+        if not input_video.is_file():
+            print("ERROR: {} not found".format(input_video))
+            exit(1)
+    else:
+        input_video = Path("./0.MOV")
+        print("Using the default, {} as the input video".format(input_video))
+        if not input_video.is_file():
+            print("ERROR: {} not found".format(input_video))
+            print("You may need to download the default video file, make sure you put it in the same directory as this program:")
+            print("https://metrostate-bb.blackboard.com/webapps/blackboard/content/listContent.jsp?course_id=_131037_1&content_id=_7094184_1")
+            exit(1)
+
+    input_data = Path("./000.dat")
+    if args.data:
+        input_data = Path(args.data)
+    print("Using {} as the GPS data".format(input_data))
+    if not input_data.is_file():
+        print("ERROR: {} not found".format(input_data))
+        exit(1)
 
 def main():
-    pass
+    setup()
 
 if __name__ == "__main__":
     main()
